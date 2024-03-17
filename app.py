f02cba04b9bd6418a502adbeba7cf6c5b1a156e6
CarlosVanegas <carlosvanegas65@gmail.com>
2024-03-16 18:26:01 -0600

    Extra Puntos: Debe programar un API a manera de exponer estos datos.

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import json
 
-from flask import Flask
+from flask import Flask, jsonify, request
 from scripts.sql_script import execute_sql_query
 from scripts.script_enviar_datos_nube import upload_data_to_dynamodb
+import mysql.connector
 
 app = Flask(__name__)
+
+# Configuración de la base de datos
+db_connection = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'database_clients_bam'
+}
 
 @app.route('/')
 def hello_world():
@@ -13,12 +22,6 @@ def hello_world():
 @app.route('/ejecutar-script')
 def ejecutar_script():
     # Parámetros para ejecutar el script
-    db_connection = {
-        'host': 'localhost',
-        'user': 'root',
-        'password': '',
-        'database': 'database_clients_bam'
-    }
 
     # Ruta del archivo de consulta SQL
     sql_file = 'querys/query_inciso_1.sql'
@@ -57,6 +60,39 @@ def subir_data():
     else:
         # Aquí puedes elegir cómo manejar los errores, por ejemplo:
         return f"Error al subir datos: {result['message']}", 400
+
+
+@app.route('/customer', methods=['GET'])
+def get_customer():
+    customer_id = request.args.get('customerId')
+    if not customer_id:
+        return jsonify({'error': 'No se proporcionó el customerId'}), 400
+
+    connection = mysql.connector.connect(**db_connection)
+    try:
+        cursor = connection.cursor(dictionary=True)
+        # Modifica esta consulta para hacer JOIN con otras tablas según lo necesites
+        sql = """
+                SELECT 
+                    c.CustomerID, c.AccountNumber, c.StoreID, c.TerritoryID, 
+                    p.FirstName, p.MiddleName, p.LastName, p.EmailPromotion, p.Title, 
+                    p.BusinessEntityID, p.PersonType, p.NameStyle, p.Suffix, 
+                    p.AdditionalContactInfo, p.Demographics
+                FROM database_clients_bam.customer AS c
+                JOIN database_clients_bam.person AS p ON c.PersonID = p.BusinessEntityID
+                WHERE c.CustomerID = %s
+                """
+        cursor.execute(sql, (customer_id,))
+        result = cursor.fetchone()
+        if result:
+            # Aquí podrías realizar más consultas para enriquecer `result` con datos de otras tablas
+            return jsonify(result)
+        else:
+            return jsonify({'error': 'Cliente no encontrado'}), 404
+    finally:
+        cursor.close()
+        connection.close()
+
 
 if __name__ == '__main__':
     app.run()
